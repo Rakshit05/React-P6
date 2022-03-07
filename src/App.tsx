67691b53efb0784f9b4f252b1dfb0ea0fcecb23e
Rakshit <mac@Macs-MacBook-Air.local>
2022-03-07 09:48:22 +0530

    React Practical 6

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { UserListCardWrapper } from "./components/UserListCardWrapper/UserListCardWrapper";
+
+function App() {
+  return (
+    <>
+      <UserListCardWrapper />
+    </>
+  );
+}
+
+export default App;
